8ea89e4db51aa29360869760e5f783c4a074fe8c
White Chan <420931016@qq.com>
2019-04-13 16:50:33 +0800

    keras cifar10 dataset

--- keras_CIFAR10_dataset.py
+++ keras_CIFAR10_dataset.py
@@ -17,25 +17,27 @@ y_label_test_OneHot = np_utils.to_categorical(y_label_test)
 model = Sequential()
 
 model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
-model.add(Dropout(0.25))
+model.add(Dropout(0.3))
 model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(Dropout(0.25))
+model.add(Dropout(0.3))
 model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'))
-model.add(Dropout(0.25))
+model.add(Dropout(0.3))
 model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 model.add(Dropout(0.25))
 
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.25))
+model.add(Dense(2500, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(1500, activation='relu'))
+model.add(Dropout(0.3))
 
 model.add(Dense(10, activation='softmax'))
 
