fd26a7bcd58501df6984aa1093c5d1be3eb4dee9
White Chan <420931016@qq.com>
2019-04-13 16:41:17 +0800

    keras cifar10 dataset

--- keras_CIFAR10_dataset.py
+++ keras_CIFAR10_dataset.py
@@ -1,5 +1,44 @@
 import numpy as np 
 from keras.datasets import cifar10
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, ZeroPadding2D
 
 (x_img_train, y_label_train), (x_img_test, y_label_test) = cifar10.load_data()
 
+# 预处理
+x_img_train_normalize = x_img_train.astype('float32') / 255.0
+x_img_test_normalize = x_img_test.astype('float32') / 255.0
+
+y_label_train_OneHot = np_utils.to_categorical(y_label_train)
+y_label_test_OneHot = np_utils.to_categorical(y_label_test)
+
+# 建立模型
+model = Sequential()
+
+model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+model.add(Dropout(0.25))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.25))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dropout(0.25))
+
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.25))
+
+model.add(Dense(10, activation='softmax'))
+
+# print(model.summary())
+
+# 训练
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+train_history = model.fit(x_img_train_normalize, y_label_train_OneHot, validation_split=0.2, epochs=20, batch_size=128, verbose=2)
+
+scores = model.evaluate(x=x_img_test_normalize, y=y_label_test_OneHot)
+print()
+print('acc = ', scores[1])
