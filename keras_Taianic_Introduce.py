8b0978efc291924582052a23d97210a13fb6f9cd
White Chan <420931016@qq.com>
2019-04-13 20:08:20 +0800

    update code

--- keras_Taianic_Introduce.py
+++ keras_Taianic_Introduce.py
@@ -1,8 +1,13 @@
 import urllib.request
 import os
+import numpy as np 
+import pandas as pd 
 
 url = "http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic3.xls"
 filepath = "data/titanic3.xls"
 if not os.path.isfile(filepath):
     result = urllib.request.urlretrieve(url, filepath)
     print('downloaded: ', result)
+
+all_df = pd.read_excel(filepath)
+print(all_df[:2])
