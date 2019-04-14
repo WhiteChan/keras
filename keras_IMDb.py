7d06627613b72f479930859c1142c9db28a9c1d1
White Chan <420931016@qq.com>
2019-04-14 12:49:03 +0800

    keras IMDb tar dataset

--- keras_IMDb.py
+++ keras_IMDb.py
@@ -2,8 +2,14 @@ import urllib.request
 import os
 import tarfile 
 
+# 下载IMDb数据
 url = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 filepath = "data/aclImdb_v1.tar.gz"
 if not os.path.isfile(filepath):
     result = urllib.request.urlretrieve(url, filepath)
     print('downloaded: ', result)
+
+# 解压文件
+if not os.path.exists("data/aclImdb"):
+    tfile = tarfile.open("data/aclImdb_v1.tar.gz", 'r:gz')
+    result = tfile.extractall('data/')
